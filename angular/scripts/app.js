a4a105920adb84248bcfed56726d6456cf963340
cwhis <caiwenhua1990@163.com>
2015-04-10 13:02:51 +0800

    update

--- angular/scripts/app.js
+++ angular/scripts/app.js
@@ -1,3 +1,20 @@
-;(function(){
+'use strict';
+var app = angular.module('mainApp', ['ngRoute','ngTouch']);
+app.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/users/signup.html',
+            controller: 'SignUpCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/users/login.html',
+            controller: 'LoginCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 
-}())
+   //$locationProvider.html5Mode(true);
+}]).run(function(){
+
+})
